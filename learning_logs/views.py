c16e253127accd9f1b17d799e183e3e0be6d8b7e
pratiksvaidya <texcellencecorp@gmail.com>
2018-05-21 11:50:35 -0500

    Show 404 for unauthorized new/edit entry access

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -52,7 +52,7 @@ def new_topic(request):
 @login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic"""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     
     if request.method != 'POST':
         # No data submitted, create a blank form.
@@ -73,7 +73,7 @@ def new_entry(request, topic_id):
 @login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
-    entry = Entry.objects.get(id=entry_id)
+    entry = get_object_or_404(Entry, id=entry_id)
     topic = entry.topic
     
     # Make sure the topic belongs to the current user.
