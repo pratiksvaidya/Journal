f06b179211ee3e54a5c686ad3e1926cf3e1f1041
pratiksvaidya <texcellencecorp@gmail.com>
2018-05-21 11:45:53 -0500

    Show 404 for unauthorized topic access

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k8mpo*wz_sms%5evio7bcm(-y2yri$l)isfv2i*skeg9b5iczv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
